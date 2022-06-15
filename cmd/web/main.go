5961e02cfaf37f4cb2b7d19f7e69af5a5e886b5b
Esterfano Lopes <esterfano.lopes@gmail.com>
2022-06-15 21:44:59 +0200

    initial setup

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "8080"
+
+func main() {
+	// connect to the database
+
+	// create sessions
+
+	// create channels
+
+	// create waitgroup
+
+	// set up the application config
+
+	// set up email
+
+	// listen for web connections
+
+}
